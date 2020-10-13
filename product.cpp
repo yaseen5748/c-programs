1276813a4b76bcd5a5a275ed71c31f217ff41f5e
yaseen5748 <53128093+yaseen5748@users.noreply.github.com>
2020-10-12 21:27:27 -0700

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,14 @@
+//program to find out the product of two integers
+
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+ int a,b,product;
+ cout<<"enter two numbers:"<<endl;
+ cin>>a>>b;
+ product = a*b;
+ cout<<"product = "<<product;
+}
---
+++ sizeof.cpp
@@ -0,0 +1,17 @@
+//program to find out the size of each data type
+
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+char a;
+int b;
+float c;
+double d;
+cout<<"size of char is:"<<sizeof(a)<<endl;
+cout<<"size of int is:"<<sizeof(b)<<endl;
+cout<<"size of float is:"<<sizeof(c)<<endl;
+cout<<"size of double is:"<<sizeof(d)<<endl;
+return 0;
+}
